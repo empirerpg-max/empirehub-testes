856b56631f7cd26bc97aaca2b655636ab33d8dcb
empirerpg-max <empirerpg.forum@gmail.com>
2026-07-24 15:41:34 -0300

    fix: routeTree.gen.ts no formato correto para TanStack Router sem plugin CLI

--- src/routeTree.gen.ts
+++ src/routeTree.gen.ts
@@ -1,7 +1,3 @@
-/* eslint-disable */
-// @ts-nocheck
-// Mantido manualmente para o ambiente de testes.
-
 import { Route as rootRoute } from './routes/__root'
 import { Route as IndexRoute } from './routes/index'
 import { Route as PlayIndexRoute } from './routes/play.index'
