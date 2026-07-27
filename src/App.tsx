60d0b13b724908b6fede669c280a4d33d6381830
empirerpg-max <empirerpg.forum@gmail.com>
2026-07-26 21:33:04 -0300

    fix: corrige import MiniPlayer para default export (resolve erro de build)

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { PlayProvider } from '@/lib/playContext'
 import { PlayHomePage } from '@/pages/PlayHomePage'
-import { MiniPlayer } from '@/components/MiniPlayer'
+import MiniPlayer from '@/components/MiniPlayer'
 
 export default function App() {
   return (
     <div className="dark min-h-screen bg-background text-foreground flex flex-col">
       <PlayProvider>
         <PlayHomePage />
-        <MiniPlayer />
+        <MiniPlayer media={null} onClose={() => {}} />
       </PlayProvider>
     </div>
   )
